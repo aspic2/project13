c8b967be03494c22ac1b7d77b1256d88ec6fa820
Thomp <thomp@protonmail.com>
2018-03-22 16:01:30 -0500

    adding the get_list_of() method for SAS

--- project13/sas.py
+++ project13/sas.py
@@ -1,6 +1,7 @@
 """Module to hold all the SAS methods."""
 
-import secrets
+import sensitive
+import requests
 
 class SAS(object):
 
@@ -13,4 +14,17 @@ class SAS(object):
     def build_url(self):
         """Add on all the subdirectories you need."""
         self.url = self.base_path
+        return self
+
+
+    def get_list_of(self, object, search_term):
+        """This will be the ajax method for auto-filling the web form
+        It needs to be able to retrieve advertisers, campaigns, and
+        flights/placements.
+        """
+        # Construct a url and send as request
+        url = self.base_path
+
+
+        response = requests.get(url, params=params, auth=(self.api_key, ''))
         return self
