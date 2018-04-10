fab739caf9958253b0ca40b010f3531da8e63580
Thomp <thomp@protonmail.com>
2018-04-10 16:01:29 -0500

    added /get/ method to support in-form ajax and auto-complete

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ import json
 # TODO: ADD dfareporting_utils to project
 #import dfareporting_utils
 #from oauth2client import client
-import flask
+from flask import Flask, redirect, render_template
 
 from project13.sas import SAS
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 #TODO: ADD adv.json to project
 #Dictionary of all advertisers and their corresponding 3rd party data
@@ -26,20 +26,32 @@ def build_report():
     # TODO: they do not need to be reloaded after every call to this page
     client_xml = getcwd() + "/resources/advertisers.xml"
     advertisers = list(advertiser_data.keys())
-    return flask.render_template("report.html", advertisers=advertisers)
+    return render_template("report.html", advertisers=advertisers)
 
 @app.route("/report")
 def run_report():
     sas_report = SAS()
     # test values
     results = sas_report.get_list_of("campaigns", "2018")
-    return flask.render_template("results.html", results=results)
+    return render_template("results.html", results=results)
+
+# TODO: maybe make this two parts? pass in object, then query?
+@app.route("/get/<query>/")
+def get(query):
+    #query = request.args.get('query')
+    #query = "fid"
+    length = None
+    advertisers = "null"
+    if query:
+        length = len(query)
+        advertisers = SAS().get_list_of("advertisers", query)
+    return advertisers
 
 #TODO: clean up redirect routes
 @app.route("/", defaults={'path': ''})
 @app.route('/<path:path>')
 def not_found_route(path):
-    return flask.redirect('/report/new')
+    return redirect('/report/new')
 
 
 if __name__ == '__main__':
